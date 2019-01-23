2fde3914eaf6ef372d845a61d3c14d8c772ebed1
bernardo de stefano <bernardo.destefano@gmail.com>
2019-01-23 11:08:44 +0100

    add tslint airbnb config

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import * as React from "react";
-import {render} from 'react-dom';
+import * as React from 'react';
+import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 import './main.scss';
-
 
 render(
   <AppContainer>
@@ -17,7 +16,7 @@ declare let module: { hot: any };
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    const NewApp = require("./components/App").default;
+    const NewApp = require('./components/App').default;
 
     render(
       <AppContainer>
